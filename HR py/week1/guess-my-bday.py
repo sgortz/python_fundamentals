7cb55519457488c4c45606d8959f5e3d15093c0c
Sabrina Gortz <sabrina.gortz@gmail.com>
2022-07-07 16:57:56 -0300

    Guesses only 4 times before giving up

--- HR py/week1/guess-my-bday.py
+++ HR py/week1/guess-my-bday.py
@@ -1,16 +1,24 @@
+# REQUIREMENTS:
+# Asks your name with the prompt "Hi! What is your name?"
+# f the computer guesses it correctly because you respond yes, it prints the message I knew it! and stops guessing
+
 from random import randint
 
 name = input("What's your name? ")
 guess = 1
-month_list = []
-year_list = []
 
 while True:
     random_month = randint(1, 12)
-    random_year = randint(1989, 2022)
+    random_year = randint(1924, 2004)
     user_answer = input(
-        f'Guess {guess}: {name}, were you born in {random_month}/{random_year}? (yes / no)'
+        f'Guess {guess}: {name}, were you born in {random_month}/{random_year}? \n (yes / no) '
     )
     if user_answer == 'yes':
+        print('I knew it')
         break
-
+    elif user_answer == 'no' and guess < 4:
+        guess += 1
+        print('Drat! Lemme try again!')
+    else:
+        print('I have other things to do. Good bye.')
+        break
